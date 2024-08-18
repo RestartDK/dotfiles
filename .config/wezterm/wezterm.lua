ac40a0a8f3f04e8f5f7367c6154600e7b7364090
R3call <thenoobyzombie@gmail.com>
2024-08-18 14:52:29 +0200

    chore: moved wezterm config to seperate dir

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,10 +5,11 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
-config.color_scheme = "Kanagawa (Gogh)"
-
+config.color_scheme = "kanagawabones"
 config.font = wezterm.font("MesloLGL Nerd Font Mono")
 config.font_size = 14
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 10
 
 config.enable_tab_bar = false
 
