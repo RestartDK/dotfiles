75105216e13826956ed00e55a6374e17c62fe59f
R3call <thenoobyzombie@gmail.com>
2024-10-20 23:17:45 +0200

    feat: move blocks in visual mode

--- .config/nvim/lua/daniel/core/keymaps.lua
+++ .config/nvim/lua/daniel/core/keymaps.lua
@@ -8,6 +8,12 @@ keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
+-- move lines visually up and down
+keymap.set("n", "J", ":m .+1<CR>==") -- move line up(n)
+keymap.set("n", "K", ":m .-2<CR>==") -- move line down(n)
+keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- move line up(v)
+keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- move line down(v)
+
 -- window management
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window vertically
