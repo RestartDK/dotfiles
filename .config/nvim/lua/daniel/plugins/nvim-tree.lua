f28000ad21bbce72ca96aaff27e63b532d29fb38
R3call <thenoobyzombie@gmail.com>
2024-10-20 23:21:32 +0200

    perf: ignore treesitter for node_modules

--- .config/nvim/lua/daniel/plugins/nvim-tree.lua
+++ .config/nvim/lua/daniel/plugins/nvim-tree.lua
@@ -43,6 +43,11 @@ return {
       git = {
         ignore = false,
       },
+      filesystem_watchers = {
+        ignore_dirs = {
+          "node_modules",
+        },
+      },
     })
 
     -- set keymaps
@@ -52,5 +57,5 @@ return {
     keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
     keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
     keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
-  end
+  end,
 }
