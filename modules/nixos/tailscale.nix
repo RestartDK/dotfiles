2f9b4ce3f33b8e68be6fba9f4c6c3a6decd1c4f5
Daniel Kumlin <danielkumlinwork@gmail.com>
2026-06-01 01:48:47 +0200

    Declare Nana Tailscale preferences

--- modules/nixos/tailscale.nix
+++ modules/nixos/tailscale.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   services.tailscale = {
@@ -9,4 +9,26 @@
   networking.firewall.trustedInterfaces = [ "tailscale0" ];
   networking.firewall.allowedUDPPorts = [ 41641 ];
   networking.firewall.checkReversePath = "loose";
+
+
+  # Keep non-secret Tailscale preferences reproducible. Authentication remains
+  # stateful in /var/lib/tailscale and is intentionally not committed.
+  systemd.services.tailscale-preferences = {
+    description = "Apply Nana Tailscale preferences";
+    after = [ "tailscaled.service" ];
+    wants = [ "tailscaled.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+    script = ''
+      ${pkgs.tailscale}/bin/tailscale set \
+        --hostname=srv-nana \
+        --accept-dns=true \
+        --accept-routes=true \
+        --operator=dkumlin
+    '';
+  };
+
 }
