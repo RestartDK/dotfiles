d794c7d4a09126e127bb613b1830ef7f5f4a28c4
Daniel Kumlin <danielkumlinwork@gmail.com>
2026-06-09 19:29:13 +0200

    Pin nix-darwin to 26.05

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     home-manager.url = "github:nix-community/home-manager/release-26.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-26.05";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
