3aa6c945267da33b28eaa322e54d17490d6975ad
Daniel Kumlin <danielkumlinwork@gmail.com>
2026-06-15 09:53:52 +0200

    Add dkumlin twin MacBook nix-darwin config

---
+++ hosts/dkumlin-twin-macbook-pro/home.nix
@@ -0,0 +1,27 @@
+{ ... }:
+
+{
+  imports = [
+    ../../modules/home/dev-packages.nix
+    ../../modules/home/live-symlinks.nix
+  ];
+
+  home.username = "danielkumlin";
+  home.homeDirectory = "/Users/danielkumlin";
+  home.stateVersion = "26.05";
+
+  my.liveConfig = {
+    enable = true;
+    repoRoot = "/Users/danielkumlin/Projects/nix-config";
+    groups = {
+      shell = true;
+      git = true;
+      editors = true;
+      terminalTools = true;
+      ghostty = true;
+      herdr = true;
+      agents = true;
+      macos = true;
+    };
+  };
+}
