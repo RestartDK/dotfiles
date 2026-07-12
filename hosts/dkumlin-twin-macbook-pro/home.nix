cb88f543a903f7ae56f7d9324bd3973ad121a041
Daniel Kumlin <danielkumlinwork@gmail.com>
2026-07-12 13:22:30 +0200

    chore: update deps

--- hosts/dkumlin-twin-macbook-pro/home.nix
+++ hosts/dkumlin-twin-macbook-pro/home.nix
@@ -14,6 +14,42 @@
 
   programs.hunk.enable = true;
 
+  programs.ssh = {
+    enable = true;
+    enableDefaultConfig = false;
+    settings = {
+      titan = {
+        HostName = "titan";
+        Port = 2222;
+        User = "daniel";
+        # Public key selector only; private key should live in the 1Password Developer vault.
+        IdentityFile = "~/.ssh/id_ed25519.pub";
+        IdentitiesOnly = true;
+        ForwardAgent = true;
+      };
+
+      "titan-2" = {
+        HostName = "titan-2";
+        Port = 2222;
+        User = "daniel";
+        # Public key selector only; private key should live in the 1Password Developer vault.
+        IdentityFile = "~/.ssh/id_ed25519.pub";
+        IdentitiesOnly = true;
+        ForwardAgent = true;
+      };
+
+      "*" = {
+        IdentityAgent = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
+      };
+    };
+  };
+
+  xdg.configFile."1Password/ssh/agent.toml".text = ''
+    # 1Password SSH agent: expose all SSH keys from the Developer vault.
+    [[ssh-keys]]
+    vault = "Developer"
+  '';
+
   my.liveConfig = {
     enable = true;
     repoRoot = "/Users/danielkumlin/.config/dotfiles";
--- packages/herdr.nix
+++ packages/herdr.nix
@@ -5,18 +5,18 @@ let
   sources = {
     x86_64-linux = {
       asset = "linux-x86_64";
-      hash = "sha256-uWWsr/wsIvVLbmxkr3z46Yo/SsJiJjCgWZxnpLnYplQ=";
+      hash = "sha256-BD70Psur2ihGXc/x7sMYRRgVDVZ7i48gzanGyIdwZB0=";
     };
     aarch64-darwin = {
       asset = "macos-aarch64";
-      hash = "sha256-FvRlPwSR6h59K0a1sCVC8Y4bguiNqvnikAVy5btjTfg=";
+      hash = "sha256-sxNFOS0ATsHxssgh4a1gEBn6g4X+HkxpMTIetYqSB3M=";
     };
   };
   source = sources.${system} or (throw "herdr is not packaged for ${system} in this config");
 in
 stdenvNoCC.mkDerivation rec {
   pname = "herdr";
-  version = "0.7.1";
+  version = "0.7.3";
 
   src = fetchurl {
     url = "https://github.com/ogulcancelik/herdr/releases/download/v${version}/herdr-${source.asset}";
